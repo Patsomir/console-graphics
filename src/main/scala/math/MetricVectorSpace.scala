21db164450c351185f34a8618620e8ae045027a4
Patsomir <pnn246@gmail.com>
2021-06-17 17:47:26 +0300

    Separate MetricVectorSpace trait

---
+++ src/main/scala/math/MetricVectorSpace.scala
@@ -0,0 +1,33 @@
+package math
+
+import scala.math.sqrt
+
+trait MetricVectorSpace[V] {
+  def add(left: V, right: V): V
+  def scale(vec: V, scaler: Float): V
+  def dot(left: V, right: V): Float
+
+  def invert(vec: V): V = scale(vec, -1)
+  def sub(left: V, right: V): V = add(left, invert(right))
+  def to(left: V, right: V): V = sub(right, left)
+  def length(vec: V): Float = sqrt(dot(vec, vec)).toFloat
+  def normalize(vec: V): V = scale(vec, 1 / length(vec))
+}
+
+object MetricVectorSpace {
+  def apply[V](implicit ms: MetricVectorSpace[V]): MetricVectorSpace[V] = ms
+
+  object ops {
+    implicit class MetricVector[V](vec: V)(implicit ms: MetricVectorSpace[V]) {
+      def +(other: V): V = ms.add(vec, other)
+      def *(scaler: Float): V = ms.scale(vec, scaler)
+      def dot(other: V): Float = ms.dot(vec, other)
+
+      def invert: V = ms.invert(vec)
+      def -(other: V): V = ms.sub(vec, other)
+      def to(other: V): V = ms.to(vec, other)
+      def length: Float = ms.length(vec)
+      def normalize: V = ms.normalize(vec)
+    }
+  }
+}
