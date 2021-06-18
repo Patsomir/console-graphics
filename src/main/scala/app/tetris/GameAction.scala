9e189b144d860deb872d0bce52e6fb530f67e43c
Patsomir <pnn246@gmail.com>
2021-06-18 22:08:12 +0300

    Add GameAction

---
+++ src/main/scala/app/tetris/GameAction.scala
@@ -0,0 +1,9 @@
+package app.tetris
+
+sealed trait GameAction
+case object MoveLeft extends GameAction
+case object MoveRight extends GameAction
+case object MoveDown extends GameAction
+case object RotateLeft extends GameAction
+case object RotateRight extends GameAction
+case object Place extends GameAction
