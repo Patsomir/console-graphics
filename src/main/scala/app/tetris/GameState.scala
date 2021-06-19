4195e2f48f6d47573e4ca47fe0119b5f04be3e99
Patsomir <pnn246@gmail.com>
2021-06-19 22:39:25 +0300

    Add occupied points extraction on GameState

--- src/main/scala/app/tetris/GameState.scala
+++ src/main/scala/app/tetris/GameState.scala
@@ -3,6 +3,15 @@ package app.tetris
 case class GameState(board: Board, activeTetromino: Tetromino, cursorRow: Int, cursorCol: Int) {
   def newActiveTetromino(tetromino: Tetromino): GameState =
     copy(cursorRow = board.width / 2 - tetromino.size / 2, cursorCol = board.height)
+
+  def occupiedPoints: List[GridPoint[TetrominoShape]] =
+    board.mapPointsWith(_ match {
+      case GridPoint(row, col, Some(shape)) => Some(GridPoint(row, col, shape))
+      case _ => None
+    }) ++ activeTetromino.mapPointsWith(_ match {
+      case GridPoint(row, col, true) => Some(GridPoint(row, col, activeTetromino.shape))
+      case _ => None
+    })
 }
 
 object GameState {
